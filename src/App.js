14e73de44d395075cc306af060022724b12c538a
Alexandr Moroz <log.koyd@gmail.com>
2023-04-19 14:23:25 +0300

    Sort + search

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState} from 'react';
 import './components/styles/App.css';
-// import PostItem from './components/postItem';
 import PostList from './components/postList';
 import PostForm from './components/UI/PostForm';
+import MySelect from './components/UI/Select/MySelect';
+import MyInput from './components/UI/Input/Myinput';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -11,7 +12,18 @@ function App() {
     { id: 3, title: 'HTML+CSS', body: 'Description3' },
   ]);
 
-  // const bodyInputRef = useRef();
+const [selectedSort, setSelectedSort] = useState('')
+const [searchQuery, setSeachQuery] = useState('')
+
+function getSortedPosts() {
+  console.log('sssssssss')
+  if (selectedSort) {
+    return [...posts].sort((a, b) => a [selectedSort].localeCompare(b [selectedSort]))
+  }
+  return posts;
+}
+
+const sortedPosts = getSortedPosts();
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
   };
@@ -20,11 +32,33 @@ function App() {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
 
+const sortPost = (sort) => {
+  setSelectedSort(sort);
+
+}
+
   return (
-    <div className="App">
+    <div className='App'>
       <PostForm create={createPost} />
+      <hr style={{margin: '15px'}} />
+      <div>
+        <MyInput
+        value={searchQuery}
+        onChange={e => setSeachQuery(e.target.value)}
+        placeholder='Search...'
+        />
+        <MySelect 
+        value={selectedSort}
+        onChange={sortPost}
+        defaultValue={'Сортировка'}
+        option={[
+          {value: 'title', name: 'По названию'},
+          {value: 'body', name: 'По описанию'},
+        ]}
+        />
+      </div>
       {posts.length !== 0 ? (
-        <PostList remove={removePost} posts={posts} title="Список постов 1" />
+        <PostList remove={removePost} posts={sortedPosts} title='Список постов 1' />
       ) : (
         <h1 style={{ textAlign: 'center' }}> Посты не найдены!</h1> 
         // Когда нет постов
