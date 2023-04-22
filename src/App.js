a800a9a8082f74f16286162c2fc99e9107c60816
Alexandr Moroz <log.koyd@gmail.com>
2023-04-22 17:21:38 +0300

    Castom hook fetch + API + detected error

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
 import './components/styles/App.css';
 import PostList from './components/postList';
 import PostForm from './components/UI/PostForm';
 import PostFilter from './components/PostFilter';
 import MyModal from './components/MyModal/MyModal.jsx';
 import MyButton from './components/UI/Button/MyButton';
-import { usePosts } from './hoocks/usePost';
+import { usePosts } from './hoocks/usePosts';
+import PostService from './API/PostService';
+import Loader from './components/UI/Loader/Loader';
+import { useFetching } from './hoocks/useFetching';
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [filter, setFilter] = useState({ sort: '', query: '' });
+  const [modal, setModal] = useState(false);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  // const [isPostsLoading, setIsPostsLoading] = useState(false);
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
 
-  
-const [filter, setFilter] = useState({sort: '', query:''})
-const [modal, setModal] = useState(false);
-const sortedAndSeachedPosts = usePosts(posts, filter.sort, filter.query); 
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -27,13 +38,25 @@ const sortedAndSeachedPosts = usePosts(posts, filter.sort, filter.query);
 
   return (
     <div className="App">
-      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>Создать пользователя</MyButton>
+      <MyButton style={{ marginRight: 10 }} onClick={fetchPosts}>
+        GET POST
+      </MyButton>
+      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
+        Создать пользователя
+      </MyButton>
       <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />{' '}
       </MyModal>
       <hr style={{ margin: '15px' }} />
       <PostFilter filter={filter} setFilter={setFilter} />
-      <PostList remove={removePost} posts={sortedAndSeachedPosts} title="Список постов 1" />
+      {postError && <h1>Ошибка ${postError}</h1>}
+      {isPostsLoading ? (
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: 50 }}>
+          <Loader />
+        </div>
+      ) : (
+        <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов 1" />
+      )}
     </div>
   );
 }
